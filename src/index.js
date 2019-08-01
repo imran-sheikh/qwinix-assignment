aa282c59bb470e750045907d497958737873ed26
Imran Sheikh <imran2604@gmail.com>
2019-08-01 23:40:00 +0530

    Added axios for APi calls, connection with APi, modified functional component to class component to set state in order to display pins and added semantic UI for showing pins in steps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Show Pins</div>
+import Api from './apis/api';
+
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       pinsCollected: false
+     }
+     this.showPins = '';
+   }
+  componentDidMount() {
+     Api
+     .get("/pins")
+     .then(async res => {
+       let dataReceived = Object.values(res.data);
+       console.log(dataReceived);
+       this.showPins = await dataReceived.map(val => <div key={val} class="ui steps"><div class="step">{val}</div></div>)
+        this.setState({pinsCollected: true})
+     })
+     .catch(err => console.log(err))
+
+  }
+  render() {
+    const containerDiv = this.state.pinsCollected ? this.showPins : <div class="ui active centered inline loader"></div>
+    return (
+      <div className="ui container">
+        <h2 className="header">1000 UNIQUE PINS Below</h2>
+        <br/>
+        {containerDiv}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
